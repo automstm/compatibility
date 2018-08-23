c337dddbc950c0ee1e83495520ffdf384f740fbf
automstm <automstm@gmail.com>
2018-08-23 20:09:24 +0530

    Working fine

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,41 +1,46 @@
-require 'watir-webdriver'
+require 'watir'
+# require 'watir-webdriver'
 require 'selenium-webdriver'
 require 'cucumber'
-
 
 Before do |scenario|
   p scenario
 
   p 'Opening the browser'
 
-
-  capabilities = Selenium::WebDriver::Remote::Capabilities.new(:timeout => 200, :javascript_enabled=>true)
+  capabilities = Selenium::WebDriver::Remote::Capabilities.new(:timeout => 300, :javascript_enabled=>true)
 
   $proxy= 'http://singtelproxy.net.vic'
   capabilities['proxy'] = Selenium::WebDriver::Proxy.new :pac => $proxy
 # require 'watir' b=Watir::Browser.start 'http://singtelproxy.net.vic:Gomes1989'
 
   case ENV['browsername']
-    when "chrome61"
+    when "chrome68"
       capabilities['platform'] = 'WINDOWS'
       capabilities['reportName'] = 'chrome'
       capabilities['browserName'] = 'chrome'
-      capabilities['version'] = '61'
+      capabilities['version'] = '68'
       # Selenium::WebDriver::Chrome.driver_path = "N:\\Shared\\Ruby\\Singtel\\features\\chromedriver.exe"
 
       Selenium::WebDriver::Chrome.driver_path = Dir.pwd+"\\features\\support\\chromedriver.exe"
-      @browser = Watir::Browser.new :chrome
+      # @browser = Watir::Browser.new :chrome
+      @browser = Watir::Browser.new(:remote,
+      :url => "http://localhost:4444/wd/hub",
+      :desired_capabilities => Selenium::WebDriver::Remote::Capabilities.chrome)
+
       # Watir::Browser.start "http://:{#$proxy}@"
-    when "firefox54"
-      # capabilities['platform'] = 'WINDOWS'
-      # capabilities['reportName'] = 'firefox'
-      # capabilities['browserName'] = 'firefox'
-      # capabilities['version'] = '54'
+    when "firefox61"
+      capabilities['platform'] = 'WINDOWS'
+      capabilities['reportName'] = 'firefox'
+      capabilities['browserName'] = 'firefox'
+      capabilities['version'] = '61'
       Selenium::WebDriver::Firefox.driver_path = "N:\\Shared\\Ruby\\Singtel\\features\\geckodriver.exe"
-      # profile = Selenium::WebDriver::Firefox::Profile.new
+
       # profile["network.proxy.type"] = 2
       # profile.proxy = Selenium::WebDriver::Proxy.new :pac => $proxy
+
       @browser = Watir::Browser.new :firefox
+
     else
       p 'Please specify browser name in command eg: browsername=chrome61'
   end
